afe4eb5968d2543725bc31c02da347db0bdd70aa
Jaloliddin <122007173+Jaloliddinnet@users.noreply.github.com>
2023-06-26 19:07:28 +0500

    add

---
+++ Hospital.Servise/DTOs/SicknesseDtos/SicknesseAddDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Servise.DTOs.SicknesseDtos
+{
+    internal class SicknesseAddDto
+    {
+    }
+}
